5b6fa7892507c6441be997bca736d591bf7ec92f
전찬혁 <jchn0111@naver.com>
2023-11-02 18:19:18 +0900

    #10 - db 리포지토리 올려야하는데 실수로 data.sql 올림
    제곧내다

--- src/main/java/com/example/boardpractice/repository/ArticleCommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.boardpractice.repository;
-
-import com.example.boardpractice.domain.ArticleComment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
-}
